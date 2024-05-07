9824b67d61009fac7e0d723f8e100dc233434363
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-05-07 10:46:00 +0700

    [CHORE] Adding capacitor to react

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'id.ac.ui.cs.fedatingpacil',
+  appName: 'frontend-pacil-dating',
+  webDir: 'build'
+};
+
+export default config;
