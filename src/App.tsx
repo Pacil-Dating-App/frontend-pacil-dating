ca75fe4520c5479444ce2b91ef3fcad907541cf9
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-05-07 17:17:52 +0700

    [FEAT] Adding dummy authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './index.css'
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
 import Home from './features/Home/Home';
 import Profile from './features/Profile/Profile';
 import Matches from './features/Matches/Matches';
-
-import './index.css'
 import Login from './features/Authentication/Login';
+import { AuthProvider } from './features/Authentication/AuthContext';
 
 const App: React.FC = () => {
     return (
-      <div className="bg-white text-black">
-        <Router>
-            {/* <nav>
-                <Link to="/">
-                  <span className="m-2 text-l mb-2">Home</span>
-                </Link>
-                <Link to="/profile">
-                  <span className="m-2 text-l mb-2">Profile</span>
-                </Link>
-                <Link to="/matches">
-                  <span className="m-2 text-l mb-2">Matches</span>
-                </Link>
-            </nav> */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login"element={<Login />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/matches" element={<Matches />} />
-            </Routes>
-        </Router>
-      </div>
+      <AuthProvider>
+        <BrowserRouter>
+        <div className="bg-white text-black">
+          {/* <nav>
+              <Link to="/">
+                <span className="m-2 text-l mb-2">Home</span>
+              </Link>
+              <Link to="/profile">
+                <span className="m-2 text-l mb-2">Profile</span>
+              </Link>
+              <Link to="/matches">
+                <span className="m-2 text-l mb-2">Matches</span>
+              </Link>
+          </nav> */}
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login"element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/matches" element={<Matches />} />
+          </Routes>
+        </div>
+        </BrowserRouter>
+      </AuthProvider>
     );
 };
 
--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import gif from '../../assets/dating-story.gif'
-
+import { useAuth } from '../Authentication/AuthContext';
 
 const Home: React.FC = () => {
+    const { isAuthenticated } = useAuth();
+    const navigate = useNavigate();
+
+    // Components for authenticated and guest users
+    const AuthenticatedContent = () => (
+        <div className=''>
+            <div className='mb-8'>
+                <Link to="/matches">
+                    <button className="bg-neutral-100 text-primary-500 font-bold py-2 px-4 rounded hover:bg-gray-100 mr-2">Explore</button>
+                </Link>
+                <Link to="/explore">
+                    <button className="bg-neutral-100 text-primary-500 font-bold py-2 px-4 rounded hover:bg-gray-100 mr-2">Matches</button>
+                </Link>
+                <Link to="/profile">
+                    <button className="bg-white text-ternary-500 font-bold py-2 px-4 rounded hover:bg-gray-100 mr-2">Profile</button>
+                </Link>
+            </div>
+        </div>
+    );
+    
+    const GuestContent = () => (
+        <Link to="/login">
+            <button className="bg-neutral-100 text-blue-500 font-bold py-2 px-4 rounded hover:bg-gray-100 mr-2">Login</button>
+        </Link>
+    );
+    
     return (
         <div className="h-screen w-screen flex flex-wrap flex-row items-center text-center px-40 pb-40">
             <div className="lg:w-1/3" style={{ maxWidth: "100%", height: "auto" }}>
@@ -13,9 +39,7 @@ const Home: React.FC = () => {
                 <h1 className="text-4xl font-bold mb-2">Pacil Dating App</h1>
                 <p className="text-xl mb-4">Connect with people around you and find your perfect match.</p>
                 <div>
-                    <Link to="/login">
-                        <button className="bg-neutral-100 text-blue-500 font-bold py-2 px-4 rounded hover:bg-gray-100 mr-2">Login</button>
-                    </Link>
+                    {isAuthenticated ? <AuthenticatedContent /> : <GuestContent />}
                 </div>
             </div>
         </div>
