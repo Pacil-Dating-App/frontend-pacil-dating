f14d4d1c2e819abc17ada4fb5af17134f61a7358
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-05-08 15:59:00 +0700

    [FIX] Fixing home page

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -31,16 +31,16 @@ const Home: React.FC = () => {
     );
     
     return (
-        <div className="h-screen w-screen flex flex-wrap flex-row items-center text-center pb-40">
+        <div className="h-screen w-screen flex flex-wrap flex-row items-center text-center px-40 pb-40">
+            <div className="lg:w-1/3" style={{ maxWidth: "100%", height: "auto" }}>
+                <img className="" src={gif} alt="hero" />
+            </div>
             <div className="lg:w-2/3 bg-white text-black text-left pl-20">
                 <h1 className="text-4xl font-bold mb-2">Find Your Pacil Match</h1>
                 <p className="text-xl mb-4">Connect with people around you and find your perfect pacilians match.</p>
                 <div>
                     {isAuthenticated ? <AuthenticatedContent /> : <GuestContent />}
                 </div>
-            </div>
-            <div className="lg:w-1/3 align-left" style={{ maxWidth: "100%", height: "auto" }}>
-                <img className="" src={gif} alt="hero" />
             </div>
         </div>
     );
